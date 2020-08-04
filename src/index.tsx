d0a62d77285e39f2b1b1873e19b32edb6e514dbd
fan <997831059@qq.com>
2020-08-04 18:12:45 +0800

    '删除一些多余的文件，更改版本号以及readme'

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import './styles/index.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(fas)
 
 export { default as Button } from './components/Button';
