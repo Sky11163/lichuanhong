76fdae61d035e43a1a02fc7d87eda08f13b3d911
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-04 11:37:25 +0000

    Deploy Sky11163/lichuanhong to github.com/Sky11163/lichuanhong.git:gh-pages

---
+++ precache-manifest.3f0a6f0900e88685ef85c95f388853b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732fae37293369dad2d1ec4016b0a33b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9dd233b2a06881dc3d65.bundle.js"
+  },
+  {
+    "url": "runtime~main.9dd233b2a06881dc3d65.bundle.js"
+  },
+  {
+    "revision": "063c1d3cdc8f706ad833",
+    "url": "static/css/main.1085fd86.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.9dd233b2a06881dc3d65.bundle.js"
+  },
+  {
+    "url": "vendors~main.9dd233b2a06881dc3d65.bundle.js.LICENSE.txt"
+  }
+]);
